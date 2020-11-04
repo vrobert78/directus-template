eb9817bce596cad1f63562e2c482be86f61a13d5
Vincent ROBERT <vincent.robert@platform.sh>
2020-11-04 20:55:48 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+cd /app
+
 set +e
 npx directus database install &>/dev/null
 if [ "$?" == "0" ] ; then
     set -e
-    cd /app
-    npx directus database install
+    echo "Database installed"
     ROLE=`npx directus roles create --name admin --admin`
+    echo "Creating administrator role"
     npx directus users create --email admin@directus.com --password 123456 --role $ROLE
+    echo "Creating administrator user"
+    timeout 60
 else
     set -e
     echo "Already initialized"
 fi
-echo "Checking database connection"
-timeout ${DB_TIMEOUT:-"30"} bash -c 'until nc -z -w 1 "$0" $1; do sleep 1; done' "${DB_HOST}" ${DB_PORT}
