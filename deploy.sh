91cf1ec00c5b58096463ed70243d84b61dc9c89a
Vincent ROBERT <vincent.robert@platform.sh>
2020-11-04 21:50:46 +0100

    Pick a version + remove init folder

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd /app
+
+set +e
+npx directus database install &>/dev/null
+if [ "$?" == "0" ] ; then
+    set -e
+    echo "Database installed"
+    ROLE=`npx directus roles create --name admin --admin`
+    echo "Creating administrator role"
+    npx directus users create --email admin@directus.com --password 123456 --role $ROLE
+    echo "Creating administrator user"
+else
+    set -e
+    echo "Already initialized"
+fi
